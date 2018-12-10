2f0de559a29fd537410de78c1a7059777a3de591
ciro golden <cgoldennlo@gmail.com>
2018-12-09 22:06:49 -0800

    adding cookie Parsers

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
+const path = require("path");
 const express = require("express");
 const logger = require("morgan");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
 app.set("view engine", "ejs");
 app.use(logger("dev"));
 
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
+
+app.use((req, res, next) => {
+    console.log("🍪 Cookies:", req.cookies);
+})
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
