7d85aa862648a32614835b3ec431fd1f8ee5a786
ciro golden <cgoldennlo@gmail.com>
2018-12-09 20:43:47 -0800

    Adding EJS/Header/Footer/Index.ejs

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 
 const app = express();
 
-
+app.set("view engine", "ejs");
 app.use(logger("dev"));
 
 
@@ -11,7 +11,13 @@ app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
+app.get("/views", (req, res) => {
+    res.render("index");
+});
 
+app.get("/views/partials", (req, res) => {
+    res.render("partials/header");
+});
 
 
 
