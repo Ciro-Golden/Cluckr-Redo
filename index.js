0b57ef727975d469dae9a8b40375075191cea6a3
ciro golden <cgoldennlo@gmail.com>
2018-12-10 23:21:31 -0800

    Adding Cookie-Parsers/Sign-In & Out FINALLY

--- index.js
+++ index.js
@@ -12,10 +12,46 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(cookieParser());
+
 
 // app.get("/", (req, res) => {
 //     res.send("/views");
 // });
+
+app.use((req, res, next) => {
+    console.log("🍪 Cookies:", req.cookies);
+
+    const username = req.cookies.username;
+
+    res.locals.username = "";
+    if (username) {
+        res.locals.username = username;
+        console.log(`😎 Signed in as ${username}`);
+    }
+    next();
+});
+
+app.get("/sign-in", (req, res) => {
+    res.render("sign-in");
+});
+
+// Routes 
+
+const COOKIE_MAX_AGE = 1000 * 60 * 60 * 24 * 7;
+app.post("/sign-in", (req, res) => {
+    const username = req.body.username;
+
+    res.cookie("username", username, { maxAge: COOKIE_MAX_AGE });
+
+    res.redirect("/views");
+
+});
+
+app.post("/sign_out", (req, res) => {
+    res.clearCookie("username");
+    res.redirect("/views");
+});
 
 app.get("/views", (req, res) => {
     res.render("sign-in");
@@ -24,6 +60,8 @@ app.get("/views", (req, res) => {
 app.get("/views/partials", (req, res) => {
     res.render("partials/header");
 });
+
+
 
 
 
